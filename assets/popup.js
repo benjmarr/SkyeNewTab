496bddf280ce978bbeabba46f3e09b6233302763
Benjamin <benjaminandyy@gmail.com>
2016-06-24 20:27:02 +1000

    v2.14.149

---
+++ assets/popup.js
@@ -0,0 +1,10 @@
+// Copyright Benjamin Developments (c) 2016 - All rights reserved
+
+setInterval(function() {
+  styleVisibility(0, "unsplash", "hidden");
+  styleVisibility(0, "options", "hidden");
+  document.getElementById("linkDiv").innerHTML = "";
+  styleVisibility(0, "linkDiv", "hidden");
+  document.getElementById("srchBx").innerHTML = "";
+  styleVisibility(0, "srchBx", "hidden");
+}, 1);

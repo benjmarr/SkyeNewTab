a9b053b71ba2114ef04d142a9e947e946bccf301
Benjamin <benjaminandyy@gmail.com>
2016-06-29 18:29:01 +1000

    v2.14.165

--- assets/background.js
+++ assets/background.js
@@ -1,7 +1,7 @@
 // Copyright Benjamin Developments (c) 2015 - All rights reserved
 
 // Random background selection script
-var backgroundNames = Array(101, 102, 103, 104, 105, 106, 107, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100);
+var backgroundNames = Array(101, 102, 103, 104, 105, 106, 107, 108, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100);
 var imageUpdateDate = "Wednesday, 29<sup>th</sup> June 2016";
 var backgroundNumber = function() {
   return backgroundNames[Math.floor(Math.random() * backgroundNames.length)];
@@ -45,15 +45,16 @@ var newBackID;
 setInterval(opChBkImgFnctn, 300000);
 
 // Background image information menu
-var backImgInfo = function(imgUrl, imgAuth, imgAuthPro, imgAuthWeb) {
+var backImgInfo = function(imgID, imgAuth, imgAuthPro, imgAuthWeb) {
   innerHTML("bkgrndImgInf1", imgAuth);
   innerHTML("bkgrndImgInf2", "<a target='_blank' href='" + imgAuthPro + "'>" + imgAuthPro + "</a>");
-  innerHTML("bkgrndImgInf3", "<a target='_blank' href='" + imgUrl + "'>" + imgUrl + "</a>");
-  if (imgAuthWeb === "" || imgAuthWeb === "web_address") {
-    innerHTML("bkgrndImgInf4", "Not available");
-  } else {
-    innerHTML("bkgrndImgInf4", "<a target='_blank' href='" + imgAuthWeb + "'>" + imgAuthWeb + "</a>");
-  };
+  innerHTML("bkgrndImgInf3", "<a target='_blank' href='https://unsplash.com/photos/" + imgID + "'>" + "https://unsplash.com/photos/" + imgID + "</a>");
+  innerHTML("bkgrndImgInf4", "<a target='_blank' href='" + imgAuthPro + "/portfolio'>Click to see</a>");
+  // if (imgAuthWeb === "" || imgAuthWeb === "web_address") {
+  //   innerHTML("bkgrndImgInf4", "Not available");
+  // } else {
+  //   innerHTML("bkgrndImgInf4", "<a target='_blank' href='" + imgAuthWeb + "'>" + imgAuthWeb + "</a>");
+  // };
 };
 document.getElementById("infoBackgroundImage").addEventListener("click", function() {
   clearMenu("true");
@@ -67,45 +68,45 @@ document.getElementById("bkgrndImgInfclose").addEventListener("click", function(
 // Background image data
 var backImageData = function() {
   if (setBackID === 101) {
-    backImgInfo("https://unsplash.com/photos/F9o7u-CnDJk", "Tony Webster", "https://unsplash.com/@tonywebster", "web_address");
+    backImgInfo("F9o7u-CnDJk", "Tony Webster", "https://unsplash.com/@tonywebster");
   } else if (setBackID === 102) {
-    backImgInfo("https://unsplash.com/photos/VBNb52J8Trk", "SpaceX", "https://unsplash.com/@spacex", "web_address");
+    backImgInfo("VBNb52J8Trk", "SpaceX", "https://unsplash.com/@spacex");
   } else if (setBackID === 103) {
-    backImgInfo("https://unsplash.com/photos/a8YV2C3yBMk", "Manik Rathee", "https://unsplash.com/@manikrathee", "web_address");
+    backImgInfo("a8YV2C3yBMk", "Manik Rathee", "https://unsplash.com/@manikrathee");
   } else if (setBackID === 104) {
-    backImgInfo("https://unsplash.com/photos/iOWEyl7sEgY", "Jimmy Musto", "https://unsplash.com/@jmust13", "web_address");
+    backImgInfo("iOWEyl7sEgY", "Jimmy Musto", "https://unsplash.com/@jmust13");
   } else if (setBackID === 105) {
-    backImgInfo("https://unsplash.com/photos/eh_vUyioHhc", "João Silas", "https://unsplash.com/@joaosilaas", "https://www.instagram.com/joaosilas");
+    backImgInfo("eh_vUyioHhc", "João Silas", "https://unsplash.com/@joaosilaas");
   } else if (setBackID === 106) {
-    backImgInfo("https://unsplash.com/photos/Hc42xXu_WOg", "veeterzy", "https://unsplash.com/@veeterzy", "https://veeterzy.com");
+    backImgInfo("Hc42xXu_WOg", "veeterzy", "https://unsplash.com/@veeterzy");
   } else if (setBackID === 107) {
-    backImgInfo("https://unsplash.com/photos/O_uHS1bru2k", "Steve Richey", "https://unsplash.com/@steverichey", "https://www.rvcmedia.com/");
-  } else if (setBackID === 88) {
-    backImgInfo("https://unsplash.com/photos/TBQXwj3DEOY", "Sujan Sundarewaran", "https://unsplash.com/@sujansundareswaran", "web_address");
+    backImgInfo("O_uHS1bru2k", "Steve Richey", "https://unsplash.com/@steverichey");
+  } else if (setBackID === 108) {
+    backImgInfo("T16-cG1gEdo", "Jimmy Musto", "https://unsplash.com/@jmust13");
   } else if (setBackID === 89) {
-    backImgInfo("https://unsplash.com/photos/c8TWWQ5ZnUw", "Hoach Le Dinh", "https://unsplash.com/@hachld", "web_address");
+    backImgInfo("c8TWWQ5ZnUw", "Hoach Le Dinh", "https://unsplash.com/@hachld");
   } else if (setBackID === 90) {
-    backImgInfo("https://unsplash.com/photos/TZCehSn-T-o/", "Anders Jildén", "https://unsplash.com/@andersjilden", "web_address");
+    backImgInfo("TZCehSn-T-o/", "Anders Jildén", "https://unsplash.com/@andersjilden");
   } else if (setBackID === 91) {
-    backImgInfo("https://unsplash.com/photos/3wbxAMUj7sg/", "Roksolana Zasiadko", "https://unsplash.com/@cieloadentro", "web_address");
+    backImgInfo("3wbxAMUj7sg/", "Roksolana Zasiadko", "https://unsplash.com/@cieloadentro");
   } else if (setBackID === 92) {
-    backImgInfo("https://unsplash.com/photos/rTZW4f02zY8/", "NASA", "https://unsplash.com/@nasa", "web_address");
+    backImgInfo("rTZW4f02zY8/", "NASA", "https://unsplash.com/@nasa");
   } else if (setBackID === 93) {
-    backImgInfo("https://unsplash.com/photos/zFnk_bTLApo", "Ismael Nieto", "https://unsplash.com/@ismaelnieto", "web_address");
+    backImgInfo("zFnk_bTLApo", "Ismael Nieto", "https://unsplash.com/@ismaelnieto");
   } else if (setBackID === 94) {
-    backImgInfo("https://unsplash.com/photos/AbTa_gQ74fs/", "Matthew Wiebe", "https://unsplash.com/@matthewwiebe", "web_address");
+    backImgInfo("AbTa_gQ74fs/", "Matthew Wiebe", "https://unsplash.com/@matthewwiebe");
   } else if (setBackID === 95) {
-    backImgInfo("https://unsplash.com/photos/qH36EgNjPJY", "Alex Siale", "https://unsplash.com/@alexsialecreative", "web_address");
+    backImgInfo("qH36EgNjPJY", "Alex Siale", "https://unsplash.com/@alexsialecreative");
   } else if (setBackID === 96) {
-    backImgInfo("https://unsplash.com/photos/kKxfELd_5pg", "Julian Lavallée", "https://unsplash.com/@julienlavallee", "web_address");
+    backImgInfo("kKxfELd_5pg", "Julian Lavallée", "https://unsplash.com/@julienlavallee");
   } else if (setBackID === 97) {
-    backImgInfo("https://unsplash.com/photos/uohGiEVhWiQ", "Artem Verbo", "https://unsplash.com/@artemverbo", "web_address");
+    backImgInfo("uohGiEVhWiQ", "Artem Verbo", "https://unsplash.com/@artemverbo");
   } else if (setBackID === 98) {
-    backImgInfo("https://unsplash.com/photos/HhCANDrFzZ0", "Priscilla Westra", "https://unsplash.com/@pwestra", "web_address");
+    backImgInfo("HhCANDrFzZ0", "Priscilla Westra", "https://unsplash.com/@pwestra");
   } else if (setBackID === 99) {
-    backImgInfo("https://unsplash.com/photos/44t1AZNIMIE", "Tobias van Schneider", "https://unsplash.com/@vanschneider", "web_address");
+    backImgInfo("44t1AZNIMIE", "Tobias van Schneider", "https://unsplash.com/@vanschneider");
   } else if (setBackID === 100) {
-    backImgInfo("https://unsplash.com/photos/sfgH9dXcMRw", "Anton Repponen", "https://unsplash.com/@repponen", "web_address");
+    backImgInfo("sfgH9dXcMRw", "Anton Repponen", "https://unsplash.com/@repponen");
   };
 };
 
